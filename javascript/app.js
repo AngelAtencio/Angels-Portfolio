d9cb5f6baad69ea2ffd611637fb3426ffa295bae
Angel Atencio <atencioangel6648@gmail.com>
2019-12-23 16:57:06 -0500

    Finished mobile nav functionality

--- javascript/app.js
+++ javascript/app.js
@@ -1,3 +1,26 @@
 particlesJS.load("particles-js", "javascript/particles.json", function() {
   console.log("callback - particles.js config loaded");
 });
+
+//Navigation menu
+const menu = document.getElementById("menu");
+const navBar = document.querySelector(".nav-elements");
+const nav = document.querySelector(".nav-bar");
+
+nav.classList.add("show");
+let isOpen = false;
+
+menu.addEventListener("click", triggerNav);
+
+function triggerNav() {
+  if (!isOpen) {
+    nav.classList.remove("show");
+
+    //Set menu state
+    isOpen = true;
+  } else {
+    nav.classList.add("show");
+    //Set menu state
+    isOpen = false;
+  }
+}
